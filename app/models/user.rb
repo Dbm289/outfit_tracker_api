339eec073e0f2d1e68e42929783e6f3f3d0f04c5
Dan <DanielMooreRealPerson@gmail.com>
2022-01-15 18:36:58 -0500

    added has_secure_passeord to user model, added user serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     has_many :comments, :dependent => :destroy
     has_many :likes, :dependent => :destroy
     has_many :ratings, :dependent => :destroy
+    has_secure_password
 
 end
