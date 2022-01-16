569138f46828cd1eed968b7fad7510daaaf1a213
Dan <DanielMooreRealPerson@gmail.com>
2022-01-15 19:05:34 -0500

    first attempt at getting outfit serializer set

--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -1,4 +1,7 @@
 class OutfitSerializer
   include JSONAPI::Serializer
   attributes :image, :description, :rating_agg, :shopping_links, :likes
+
+  has_many :shopping_links, :likes,
+  has_one :image, :description, :rating_agg
 end
