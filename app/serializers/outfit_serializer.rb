82567cc1362f7f2c69fd79fc0e34ed6cd31d25ca
Dan <DanielMooreRealPerson@gmail.com>
2022-01-17 22:54:35 -0500

    attempting to solve serializer issue, unsure what is causing it now

--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -2,6 +2,4 @@ class OutfitSerializer
   include JSONAPI::Serializer
   attributes :image, :description, :rating_agg, :shopping_links, :likes
 
-  has_many :shopping_links, :likes,
-  has_one :image, :description, :rating_agg
 end
