6375e71e201cbff51843f291fbd1651190050e1d
Dan <DanielMooreRealPerson@gmail.com>
2022-01-17 23:24:29 -0500

    adding routes to help with serializer testing

--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -1,5 +1,8 @@
 class OutfitSerializer
   include JSONAPI::Serializer
-  attributes :image, :description, :rating_agg, :shopping_links, :likes
+  attributes :id, :image, :description, :rating_agg, :shopping_links, :likes
 
+  has_many :shopping_links, :likes
+end 
+  has_one :image, :description, :rating_agg
 end
