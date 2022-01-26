b31b0f09281899baf2d609450385c99da2fcc3ff
Dan <DanielMooreRealPerson@gmail.com>
2022-01-25 23:37:43 -0500

    routes set using root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show, :create, :update, :destroy]
-  resources :outfits, only: [:index, :show, :create, :destroy]
-  resources :comments, only: [:index, :show, :create, :destroy]
-  resources :like, only: [:create, :destroy]
-  resources :rating, only: [:show, :update]
-  resources :link, only: [:show, :create, :update, :destroy]
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users 
+      resources :outfits
+      resources :comments
+      resources :likes
+      resources :links
+      resources :ratings
+
+    end
+
+  end
 end
